3407bd2dde0cd8b45171c61e1fed1ccf62e8bc9c
s1250023-Igarashi <s1250023@u-aizu.ac.jp>
2018-07-02 13:56:47 +0900

    Won/lost message added

--- diceGame.py
+++ diceGame.py
@@ -4,7 +4,13 @@ print("Rolling the dice...")
 
 die1 = random.randint(1, 6)
 die2 = random.randint(1, 6)
+total = die1 + die2
 
 print("Die 1: " + str(die1))
 print("Die 2: " + str(die2))
-print("Total value: " + str(die1 + die2))
+print("Total value: " + str(total))
+
+if total > 7:
+    print("You won")
+else:
+    print("You lost")
