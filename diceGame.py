dfc3998a9c346d18baa91fd4c08acf50bee5e0cd
s1250023-Igarashi <s1250023@u-aizu.ac.jp>
2018-07-02 13:38:52 +0900

    Initial revision

---
+++ diceGame.py
@@ -0,0 +1,10 @@
+import random
+
+print("Rolling the dice...")
+
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+
+print("Die 1: " + str(die1))
+print("Die 2: " + str(die2))
+print("Total value: " + str(die1 + die2))
